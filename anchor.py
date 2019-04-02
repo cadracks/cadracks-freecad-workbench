f6c8faf4e35762a371b25166e2313f532f18301c
Gitea <gitea@fake.local>
2019-04-02 18:14:42 +0200

    GPL v3 copyright notices, license notices and license file

--- anchor.py
+++ anchor.py
@@ -1,5 +1,22 @@
 # coding: utf-8
 
+# Copyright 2018-2019 Guillaume Florent
+
+# This file is part of cadracks-freecad-workbench.
+#
+# cadracks-freecad-workbench is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# cadracks-freecad-workbench is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cadracks-freecad-workbench.  If not, see <https://www.gnu.org/licenses/>.
+
 r"""Anchor Python Feature
 
 Is made of a point and 2 unit and orthogonal vectors
--- puv.py
+++ puv.py
@@ -1,5 +1,22 @@
 # coding: utf-8
 
+# Copyright 2018-2019 Guillaume Florent
+
+# This file is part of cadracks-freecad-workbench.
+#
+# cadracks-freecad-workbench is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# cadracks-freecad-workbench is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cadracks-freecad-workbench.  If not, see <https://www.gnu.org/licenses/>.
+
 r"""Determination of p, u, v components of anchors from part geometry"""
 
 import Part
